799fe83fe8ca3f7a749eec6939b5a35faf6dddde
itai natan <itai08425@gmail.com>
2022-05-11 23:12:25 +0300

    styling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FilterOptions, Wand } from './types';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   searchQuery = {
@@ -14,21 +14,19 @@ export class AppComponent implements OnInit {
     wood: 'All',
     length: 'All',
     core: 'All',
-    flexibility: 'All'
+    flexibility: 'All',
   };
   wands?: Observable<Wand[]>;
   options?: Observable<FilterOptions>;
   selected?: Wand;
 
-
-  constructor(private readonly http: HttpClient) {
-  }
+  constructor(private readonly http: HttpClient) {}
 
   ngOnInit() {
     this.options = forkJoin({
       core: this.http.get('http://127.0.0.1:3000/api/options/core'),
       length: this.http.get('http://127.0.0.1:3000/api/options/length'),
-      wood: this.http.get('http://127.0.0.1:3000/api/options/wood')
+      wood: this.http.get('http://127.0.0.1:3000/api/options/wood'),
     }) as Observable<FilterOptions>;
     this.fetchWands();
   }
@@ -38,13 +36,15 @@ export class AppComponent implements OnInit {
      * Task Client/1:
      * Fix me
      */
-    const {wood, term, core, length} = this.searchQuery;
+    const { wood, term, core, length } = this.searchQuery;
     console.log(`the search query is: ${JSON.stringify(this.searchQuery)}`);
-    this.wands = this.http.get<Wand[]>(`http://127.0.0.1:3000/api/wands/${term}?wood=${wood}&core=${core}&length=${length}`);
+    this.wands = this.http.get<Wand[]>(
+      `http://127.0.0.1:3000/api/wands/${term}?wood=${wood}&core=${core}&length=${length}`
+    );
   }
 
   filterSearch(e: { [key: string]: string } | any) {
-    this.searchQuery = {...this.searchQuery, ...e};
+    this.searchQuery = { ...this.searchQuery, ...e };
     this.fetchWands();
   }
 }
